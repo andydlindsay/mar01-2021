a5832c6fae3add795fd0ea96307d524809731716
Andy Lindsay <andydlindsay@gmail.com>
2021-05-12 13:16:01 -0700

    small changes

--- breakout-react-router/routing/src/App.js
+++ breakout-react-router/routing/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Authors from './components/Authors';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Authors from './components/Authors';
 
 const App = () => {
   return (
     <div className="App">
       <h2>React Routing</h2>
-      <Home />
-      <About />
-      <Authors />
     </div>
   );
 };
